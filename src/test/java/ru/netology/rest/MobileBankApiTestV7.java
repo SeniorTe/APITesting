cd18e84fc3a218a7a9a473a6617fccc1da3a9dbb
Menshikov.SI <89089118626@mail.ru>
2023-03-06 15:32:20 +0500

    add test

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV7.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class MobileBankApiTestV7 {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[0].currency", equalTo("RUB"))
+                .body("[2].currency", equalTo("RUB"))
+                .body("[1].currency", equalTo("USD"))
+        ;
+    }
+}
